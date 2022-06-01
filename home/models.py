09b4a287f1de21381539a7789e8b00b87e85fc0a
Savvy Insights <greg@savvyinsights.io>
2022-06-01 15:15:28 -0400

    homepage

--- home/models.py
+++ home/models.py
@@ -1,13 +1,19 @@
 from django.db import models
 
-from wagtail.models import Page
+#from wagtail.models import Page
+from wagtail.core.models import Page
 from wagtail.fields import RichTextField
-from wagtail.admin.panels import FieldPanel
+from wagtail.admin.edit_handlers import FieldPanel
 
 
 class HomePage(Page):
+    """Home page model """
+
+    templates = "home/home_page.html"
+
+    banner_title = models.CharField(max_length=100, blank=False, null=True)
     body = RichTextField(blank=True)
 
     content_panels = Page.content_panels + [
-        FieldPanel('body', classname="full"),
+        FieldPanel('banner_title', classname="full"),
     ]
